665d419e7988ecf263dd2c26ced3db00d6df37c6
SHAKIL AHMED <88330594+shakilahmedemon@users.noreply.github.com>
2026-01-16 23:57:27 +0800

    Initial Commit
    Added a Vision Transformer model for skin cancer classification with dropout.

--- model/vit_model.py
+++ model/vit_model.py
@@ -1,1 +1,18 @@
+import torch
+import torch.nn as nn
+import timm
+
+class SkinCancerViT(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model = timm.create_model(
+            "vit_base_patch16_224",
+            pretrained=True,
+            num_classes=num_classes
+        )
+        self.dropout = nn.Dropout(p=0.3)
+
+    def forward(self, x):
+        x = self.dropout(x)
+        return self.model(x)
 
